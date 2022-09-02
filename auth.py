8b8037f76036bbe606b6c1d4eb7ff880db252faa
Muluken Menberu <mulukenmenberu@intake.education>
2022-09-02 17:57:32 +0300

    documentation added

--- auth.py
+++ auth.py
@@ -4,14 +4,19 @@ from datetime import datetime, timedelta
 from jose import JWTError, jwt
 
 from error import HttpError
+
+##JWT Secret and Algorithm
 SECRET_KEY = "testsecret"
 ALGORITHM = "HS256"
 
+## This will generate a new token upon successfull login
 def generate_jwt_token(user_data={"name":"Mule","role":"admin"}):
     expire_time = datetime.utcnow() + timedelta(minutes=45)
     user_data.update({"exp": expire_time})
     encoded_jwt = jwt.encode(user_data, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
+
+## This will validate the token sent from the client
 def verify_jwt_token(token, user='all'):
     token_array = token.split(' ')[1]
     try:
@@ -28,6 +33,7 @@ def verify_jwt_token(token, user='all'):
             detail="Could not validate credentials",
         )
 
+## Decorator to be user for common apis (for apis used by both Admin & user)
 def login_required(f):
     @wraps(f)
     def wrapper(*args, **kwargs):
@@ -41,6 +47,7 @@ def login_required(f):
     return wrapper
     
 
+## Decorator to check Admin Access
 def admin_login_required(f):
     @wraps(f)
     def wrapper(*args, **kwargs):
@@ -52,6 +59,8 @@ def admin_login_required(f):
         return f(payload, *args, **kwargs)
 
     return wrapper
+
+##Decorator to check user access
 def user_login_required(f):
     @wraps(f)
     def wrapper(*args, **kwargs):
